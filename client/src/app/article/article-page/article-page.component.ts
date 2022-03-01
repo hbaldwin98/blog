dda56374890b3e2518af94d7da2cb74475a2cef1
Hunter <huntercbaldwin98@gmail.com>
2022-02-28 18:56:19 -0800

    U.I. enhancement for article and comment posts

--- client/src/app/article/article-page/article-page.component.ts
+++ client/src/app/article/article-page/article-page.component.ts
@@ -1,9 +1,11 @@
+import { UserComment } from 'src/app/_models/usercomment';
 import { CommentService } from './../../_services/comment.service';
 import { PostComment } from './../../_models/postComment';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ExtraOptions } from '@angular/router';
 import { Article } from 'src/app/_models/article';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-article-page',
@@ -12,16 +14,26 @@ import { Article } from 'src/app/_models/article';
 })
 export class ArticlePageComponent implements OnInit {
   @Input() article!: Article;
+  comments!: UserComment[];
   commentForm!: FormGroup;
   newComment!: PostComment;
 
-  constructor(private route: ActivatedRoute, private fb: FormBuilder, private commentService: CommentService) { }
+  constructor(private route: ActivatedRoute, private fb: FormBuilder, private commentService: CommentService, private viewportScroller: ViewportScroller) { }
 
   ngOnInit(): void {
     this.initializeForm();
     this.route.data.subscribe((data => {
       this.article = data.article;
+      this.comments = data.article.comments.sort((a: UserComment, b: UserComment) => new Date(b.dateCommented).getTime() - new Date(a.dateCommented).getTime());
     }))
+
+    this.route.fragment.subscribe(x => {
+      if (x === "comments") {
+        setTimeout(() => {
+          this.scrollToComments();
+        }, 200);
+      }
+    });
   }
 
   initializeForm() {
@@ -31,13 +43,32 @@ export class ArticlePageComponent implements OnInit {
     })
   }
 
+  /**
+   * TODO: This whole process needs to be changed. Comments should not be loaded until the page has been accessed.
+   * TODO: They should be in their own service and updated accordingly everytime the page is accessed as new comments
+   * TODO: could be sent after leaving a page. This sort of works now, however, for testing purposes.
+   * @memberof ArticlePageComponent
+   */
   postComment() {
     this.newComment = {
       commenterName: this.commentForm.value['commenterName'],
       contents: this.commentForm.value['content']
     }
 
-    this.commentService.postComment(this.newComment, this.article.urlIdentity);
+    if (this.commentService.postComment(this.newComment, this.article.urlIdentity)) {
+      let comment: UserComment = {
+        commenterName: this.newComment.commenterName,
+        contents: this.newComment.contents,
+        dateCommented: new Date(Date.now())
+      }
+
+      this.comments.push(comment)
+      this.comments.sort((a: UserComment, b: UserComment) => new Date(b.dateCommented).getTime() - new Date(a.dateCommented).getTime());
+    }
+  }
+
+  scrollToComments() {
+    this.viewportScroller.scrollToAnchor('comments');
   }
 
 }
